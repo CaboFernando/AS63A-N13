93992782a1ac47a1f549a65c7053a4dac56deb8d
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2025-05-16 20:20:34 -0300

    Criando conexão com mongo

---
+++ Aula-7/app.js
@@ -0,0 +1,9 @@
+const Usuario = require("./usuarios");
+
+async function testarInsercao() {
+    const user = new Usuario("Monique", "monique@example.com");
+
+    await user.inserir();
+}
+
+testarInsercao();
